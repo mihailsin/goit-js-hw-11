c83df2103428e21955b9aad15a228376f742685a
Mihail Sinkov <mihailsin88@gmail.com>
2021-11-14 22:59:45 +0200

    initiates code refactoring with classes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import SimpleLightbox from 'simplelightbox';
 import Notiflix, { Notify } from 'notiflix';
 import { form, searchInput, button, gallery } from './js/refs';
-import { fetchPictures } from './js/request';
+import { PixabayApi } from './js/request';
 import { createCards } from './js/markup';
 const ALERT_STRING = 'Sorry, there are no images matching your search query. Please try again.';
 let query;
@@ -38,44 +38,44 @@ function onSubmit(e) {
   button.removeAttribute('disabled');
 }
 
-async function renderPictures() {
-  const KEY = '24271792-2ae9c4be49492e469cc4e2f34';
-  const REQUEST_PARAMS = 'image_type=photo&orientation=horizontal&safesearch=true';
-  const BASE_URL = 'https://pixabay.com/api/';
-  try {
-    gallery.innerHTML = '';
-    const pictures = await fetchPictures(
-      `${BASE_URL}?key=${KEY}&q=${query}&${REQUEST_PARAMS}&page=${page}&per_page=40`,
-    );
-    const cards = await createCards(pictures.hits);
-    const markup = await addMarkup(cards);
-    lightbox = new SimpleLightbox('.photo-card a', {
-      captionsData: 'alt',
-      captionPosition: 'bottom',
-      captionDelay: 500,
-    });
-    lightbox.refresh();
-    let totalHits = pictures.totalHits;
-    let responseQuantity = pictures.hits.length;
-    const STRING_OF_JOY = `Hooray! We found ${totalHits} images.`;
-    if (page === 1 && responseQuantity > 1) {
-      Notiflix.Notify.success(STRING_OF_JOY);
-    }
-    if (responseQuantity < 40 && responseQuantity > 0) {
-      button.setAttribute('disabled', true);
-    } else if (responseQuantity === 0) {
-      Notiflix.Notify.failure(ALERT_STRING);
-      button.classList.add('visually-hidden');
-    } else {
-      button.classList.remove('visually-hidden');
-    }
-    return markup;
-  } catch ({ name, message, stack }) {
-    console.log(name);
-    console.log(message);
-    console.log(stack);
-  }
-}
-function addMarkup(markup) {
-  gallery.insertAdjacentHTML('beforeend', markup);
-}
+// async function renderPictures() {
+//   const KEY = '24271792-2ae9c4be49492e469cc4e2f34';
+//   const REQUEST_PARAMS = 'image_type=photo&orientation=horizontal&safesearch=true';
+//   const BASE_URL = 'https://pixabay.com/api/';
+//   try {
+//     gallery.innerHTML = '';
+//     const pictures = await fetchPictures(
+//       `${BASE_URL}?key=${KEY}&q=${query}&${REQUEST_PARAMS}&page=${page}&per_page=40`,
+//     );
+//     const cards = await createCards(pictures.hits);
+//     const markup = await addMarkup(cards);
+//     lightbox = new SimpleLightbox('.photo-card a', {
+//       captionsData: 'alt',
+//       captionPosition: 'bottom',
+//       captionDelay: 500,
+//     });
+//     lightbox.refresh();
+//     let totalHits = pictures.totalHits;
+//     let responseQuantity = pictures.hits.length;
+//     const STRING_OF_JOY = `Hooray! We found ${totalHits} images.`;
+//     if (page === 1 && responseQuantity > 1) {
+//       Notiflix.Notify.success(STRING_OF_JOY);
+//     }
+//     if (responseQuantity < 40 && responseQuantity > 0) {
+//       button.setAttribute('disabled', true);
+//     } else if (responseQuantity === 0) {
+//       Notiflix.Notify.failure(ALERT_STRING);
+//       button.classList.add('visually-hidden');
+//     } else {
+//       button.classList.remove('visually-hidden');
+//     }
+//     return markup;
+//   } catch ({ name, message, stack }) {
+//     console.log(name);
+//     console.log(message);
+//     console.log(stack);
+//   }
+// }
+// function addMarkup(markup) {
+//   gallery.insertAdjacentHTML('beforeend', markup);
+// }
--- src/js/request.js
+++ src/js/request.js
@@ -1,7 +1,24 @@
 const axios = require('axios');
-export async function fetchPictures(url) {
-  const response = await axios.get(url);
-  const pictures = await response.data;
+// export async function fetchPictures(url) {
+//   const response = await axios.get(url);
+//   const pictures = await response.data;
 
-  return pictures;
+//   return pictures;
+// }
+
+export default class PixabayApi {
+  constructor() {
+    this.searchQuery = '';
+  }
+
+  async fetchPictures(url) {
+    const KEY = '24271792-2ae9c4be49492e469cc4e2f34';
+    const REQUEST_PARAMS = 'image_type=photo&orientation=horizontal&safesearch=true';
+    const BASE_URL = 'https://pixabay.com/api/';
+    const response = await axios.get(url);
+    const pictures = await response.data;
+
+    return pictures;
+  }
+  incrementPage() {}
 }
