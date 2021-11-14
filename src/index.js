12e59962d90ee7641ce6286dcff23b7f53335ef1
Mihail Sinkov <mihailsin88@gmail.com>
2021-11-14 14:08:25 +0200

    this commit snapshots some progress

--- src/index.js
+++ src/index.js
@@ -1,28 +1,49 @@
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
 import { fetchPictures } from './js/request';
-const KEY = '24271792-2ae9c4be49492e469cc4e2f34';
-const BASE_URL = 'https://pixabay.com/api/';
-const PAGINATE = 'page=1&per_page=40';
+import Notiflix from 'notiflix';
+const ALERT_STRING = 'Sorry, there are no images matching your search query. Please try again.';
 let query;
-let page;
+let page = 1;
+
 const form = document.querySelector('#search-form');
 const searchInput = document.querySelector('[name="searchQuery"]');
+const button = document.querySelector('.load-more');
+button.addEventListener('click', onClick);
+
+const gallery = document.querySelector('.gallery');
 form.addEventListener('input', onInput);
 form.addEventListener('submit', onSubmit);
+function onClick(e) {
+  page += 1;
+  renderPictures();
+}
 function onInput(e) {
   query = e.target.value;
 }
 function onSubmit(e) {
   e.preventDefault();
   renderPictures();
+  button.removeAttribute('disabled');
 }
-const gallery = document.querySelector('.gallery');
 
 async function renderPictures() {
+  const KEY = '24271792-2ae9c4be49492e469cc4e2f34';
+  const BASE_URL = 'https://pixabay.com/api/';
   try {
-    const pictures = await fetchPictures(`${BASE_URL}?key=${KEY}&q=${query}&${PAGINATE}`);
+    gallery.innerHTML = '';
+    const pictures = await fetchPictures(
+      `${BASE_URL}?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&page=${page}&per_page=40`,
+    );
+    let responseQuantity = pictures.hits.length;
     const cards = await createCards(pictures.hits);
     const markup = await addMarkup(cards);
-    return markup;
+    console.log(responseQuantity);
+    if (responseQuantity < 40 && responseQuantity > 0) {
+      button.setAttribute('disabled', true);
+    } else if (responseQuantity === 0) {
+      Notiflix.Notify.failure(ALERT_STRING);
+    } else return markup;
   } catch ({ name, message, stack }) {
     console.log(name);
     console.log(message);
@@ -32,9 +53,11 @@ async function renderPictures() {
 
 function createCards(pictures) {
   const markup = pictures
-    .map(({ webformatURL, tags, likes, views, comments, downloads }) => {
-      return `<div class="photo-card">
+    .map(({ largeImageURL, webformatURL, tags, likes, views, comments, downloads }) => {
+      return ` <div class="photo-card">
+      <a href="${largeImageURL}">
   <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  </a>
   <div class="info">
     <p class="info-item">
     Likes
--- src/js/request.js
+++ src/js/request.js
@@ -2,5 +2,6 @@ const axios = require('axios');
 export async function fetchPictures(url) {
   const response = await axios.get(url);
   const pictures = await response.data;
+
   return pictures;
 }
